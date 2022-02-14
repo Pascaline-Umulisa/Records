1027b5da004327d6a4c2870114195aef0e980a2a
Pascaline-Umulisa <kayiteteumulisa@gmail.com>
2022-02-14 08:34:59 +0300

    first commit

---
+++ records.kt
@@ -0,0 +1,21 @@
+fun main(){
+        println("STUDENT RECORD including name age phone number and nationality")
+        println("Student1")
+        var name="Pascaline Umulisa"
+        var age=26
+        println(name)
+        println(age)
+        var phonenumber = "o786938558"
+        println (phonenumber)
+
+        var weight =(50)
+        println(weight)
+
+
+        var isfromkenya:Boolean=false
+        println(isfromkenya)
+
+
+
+    }
+
